0db0b27bac639a9ff2c953b6269169d86692ef51
592874 <rahulpratap@C02T44PVH03Q.fios-router.home>
2017-09-07 14:00:45 -0400

    First Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'haproxy'
+
+template '/etc/haproxy/haproxy.cfg' do
+  source 'haproxy.cfg.erb'
+end
+
+service 'haproxy' do
+  actions [ :start, :enable ]
+end
+
